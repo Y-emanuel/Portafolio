1852bb3cc0610f04964e12764b7f7aba1d203270
Emanuel Ybalo <brianybalo70@gmail.com>
2023-11-28 17:30:24 -0300

    modificaciones de estilo

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 // script.js
-
 function smoothScroll(target) {
-    const targetElement = document.querySelector(target);
-    window.scrollTo({
-      top: targetElement.offsetTop,
+  const targetElement = document.querySelector(target);
+
+  // Obtener la altura del h2
+  const headerHeight = document.querySelector('h2').offsetHeight;
+
+  window.scrollTo({
+      top: targetElement.offsetTop - headerHeight - 5 * parseFloat(getComputedStyle(document.documentElement).fontSize),
       behavior: "smooth"
-    });
-  }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const enlacesMenu = document.querySelectorAll('.navegador__botones');
-  
-    enlacesMenu.forEach(enlace => {
-      enlace.addEventListener('click', function (event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        smoothScroll(targetId);
-      });
-    });
   });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const enlacesMenu = document.querySelectorAll('.navegador__botones');
+
+  enlacesMenu.forEach(enlace => {
+      enlace.addEventListener('click', function (event) {
+          event.preventDefault();
+          const targetId = this.getAttribute('href');
+          smoothScroll(targetId);
+      });
+  });
+});
+
   
